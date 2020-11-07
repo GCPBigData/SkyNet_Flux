b747e1b83bbfd5197a747da97c43bc4faf88fee0
Jose R F Junior <web2ajax@gmail.com>
2020-11-07 08:35:01 -0300

    upgrade

--- src/main/java/br/skynet/repository/AnimeRepository.java
+++
@@ -1,9 +0,0 @@
-package br.skynet.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Mono;
-
-public interface AnimeRepository extends ReactiveCrudRepository<Anime, Integer> {
-
-    Mono<Anime> findById(int id);
-}
